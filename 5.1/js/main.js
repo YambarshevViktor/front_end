f9a90229480c13f7a5c9699c50319e618077e71e
Yambarshev Viktor <vi.yambar@mail.ru>
2018-03-25 17:08:33 +0300

    lw5.1

---
+++ 5.1/js/main.js
@@ -0,0 +1,27 @@
+function last(){
+  var string = 'Hello, world';
+  console.log('Последний символ строки "Hello, world": ', string.slice(-1));
+}
+
+last();
+
+function nolast(){
+  var string = 'Hello, world';
+  console.log('Строка "Hello, world" без последнего символа: ', string.slice(0, -1));
+}
+
+nolast()
+
+function reverse(){
+  var string = 'Hello, world';
+  console.log('Зерклаьно отражённая строка "Hello, world": ', string.split('').reverse().join(''));
+}
+
+reverse()
+
+function extraSpaces(){
+  var string = '  Hello,      World!';
+  console.log('Строка "  Hello,      World!" без лишних пробелов: ', string.replace(/\s+/g, ' '));
+}
+
+extraSpaces()
