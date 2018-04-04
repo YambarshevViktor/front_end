2ff0d76f865bd088479999b7e2be76a530c13b79
Yambarshev Viktor <vi.yambar@mail.ru>
2018-04-04 14:46:53 +0300

    lw5.1.

--- 5.1/js/main.js
+++ 5.1/js/main.js
@@ -1,27 +1,23 @@
-function last(){
-  var string = 'Hello, world';
-  console.log('Последний символ строки "Hello, world": ', string.slice(-1));
+function last(string){
+  return string.slice(-1);
 }
 
-last();
-
-function nolast(){
-  var string = 'Hello, world';
-  console.log('Строка "Hello, world" без последнего символа: ', string.slice(0, -1));
+function nolast(string){
+  return string.slice(0, -1);
 }
 
-nolast()
-
-function reverse(){
-  var string = 'Hello, world';
-  console.log('Зерклаьно отражённая строка "Hello, world": ', string.split('').reverse().join(''));
+function reverse(string){
+  return string.split('').reverse().join('');
 }
 
-reverse()
-
-function extraSpaces(){
-  var string = '  Hello,      World!';
-  console.log('Строка "  Hello,      World!" без лишних пробелов: ', string.trim().replace(/\s+/g, ' '));
+function extraSpaces(string){
+  return string.trim().replace(/\s+/g, ' ');
 }
 
-extraSpaces()
+var lastInput = noLastInput = reverseInput = 'Hello, world';
+var extraSpacesInput = '  Hello,      World!';
+
+console.log('Последний символ строки "Hello, world":', last(lastInput));
+console.log('Строка "Hello, world" без последнего символа:',nolast(noLastInput));
+console.log('Зерклаьно отражённая строка "Hello, world":',reverse(reverseInput));
+console.log('Строка "  Hello,      World!" без лишних пробелов:', extraSpaces(extraSpacesInput));
